d68d3e7e9e1cb4df0ef52a551bb9f20f6cb44782
Kromi <33200410+4weKromi@users.noreply.github.com>
2019-07-20 09:19:42 +0530

    Update apMode001.ino

--- src/apMode001.ino
+++ src/apMode001.ino
@@ -1,21 +1,25 @@
-
-/*
-	Using Access Point (AP)
+/*	Using Access Point (AP)
 	Two Relay Switches
-  Web Interface to toggle switches
-  
-  created by 4wekromi
-*/
+	Web Interface to toggle switches
+	by 4wekromi		*/
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+
+void OnConnect();
+void SwOneOn();
+void SwOneOff();
+void SwTwoOn();
+void SwTwoOff();
+void NotFound();
+String sendHTML(uint8_t stat1, uint8_t stat2);
 
 /* Put your SSID & Password */
 const char* ssid = "kromi_home";  
 const char* password = "qwer1234";
 
 /* your static IP */
-IPAddress local_ip(192, 168, 2, 1);
-IPAddress gateway(192, 168, 2, 1);
+IPAddress local_ip(192, 168, 0, 2);
+IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
@@ -29,17 +33,17 @@ void setup() {
   Serial.begin(9600);
   pinMode(swOnePin, OUTPUT);
   pinMode(swTwoPin, OUTPUT);
-
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
-
-  server.on("/", handle_OnConnect);
-  server.on("/swone_on", handle_swone_on);
-  server.on("/swone_off", handle_swone_off);
-  server.on("/swtwo_on", handle_swtwo_on);
-  server.on("/swtwo_off", handle_swtwo_off);
-  server.onNotFound(handle_NotFound);
+  swOneState = LOW;
+  swTwoState = LOW;
+  server.on("/", OnConnect);
+  server.on("/swone_on", SwOneOn);
+  server.on("/swone_off", SwOneOff);
+  server.on("/swtwo_on", SwTwoOn);
+  server.on("/swtwo_off", SwTwoOff);
+  server.onNotFound(NotFound);
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -49,69 +53,57 @@ void loop() {
     digitalWrite(swOnePin, HIGH);
   else
     digitalWrite(swOnePin, LOW);
-
   if (swTwoState)
     digitalWrite(swTwoPin, HIGH);
   else
     digitalWrite(swTwoPin, LOW);
 }
 
-void handle_OnConnect() {
+void OnConnect() {
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  swOneState = LOW;
-  swTwoState = LOW;
+  Serial.println(local_ip);
   server.send(200, "text/html", SendHTML(false, false));
 }
 //switch 1
-void handle_swone_on() {
+void SwOneOn() {
   swOneState = HIGH;
   server.send(200, "text/html", SendHTML(true, swTwoState));
 }
-void handle_swone_off() {
+void SwOneOff() {
   swOneState = LOW;
   server.send(200, "text/html", SendHTML(false, swTwoState));
 }
 //switch 2
-void handle_swtwo_on() {
+void SwTwoOn() {
   swTwoState = HIGH;
   server.send(200, "text/html", SendHTML(swOneState, true));
 }
-void handle_swtwo_off() {
+void SwTwoOff() {
   swTwoState = LOW;
   server.send(200, "text/html", SendHTML(swOneState, false));
 }
 
-void handle_NotFound() {
+void NotFound() {
   server.send(404, "text/plain", "Not found");
 }
 
 String SendHTML(uint8_t swOne, uint8_t swTwo) {
-  String ptr = "<!DOCTYPE html>\n";
-  ptr += "<html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">\n";
-  ptr += "<head>\n";
+  String ptr = "<!DOCTYPE html>\n<html>\n";
+  ptr +="<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=yes'>\n";
   ptr += "<title>Home Control (IoT)</title>\n";
   ptr += "<style>input[type=button]{width: 50px;  height: 40px;} body{text-align: center;}</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
+  ptr += "<meta http-equiv='refresh' content='10'/></head>\n<body>\n";
   ptr += "<h2 style='color:#1f5ed3'>Welcome Home</h2><br>\n";
   ptr += "<form method=\"get\">\n";
-  if (swOne) {
-    ptr += "<p>Switch #1 : Status: ON</p><input type=\"button\" value=\"OFF\" onclick=\"window.location.href='/swone_off'\">\n";
-  }
-  else {
-    ptr += "<p>Switch #1 : Status: OFF</p><input type=\"button\" value=\"ON\" onclick=\"window.location.href='/swone_on'\">\n";
-  }
+  if (swOne||swOneState)
+    ptr += "<p>Switch #1 : Status: OFF</p><input type='button' value='ON' onclick=\"window.location.href='/swone_off'\">\n";
+  else
+    ptr += "<p>Switch #1 : Status: ON</p><input type='button' value='OFF' onclick=\"window.location.href='/swone_on'\">\n";
   ptr += " <br><br>";
-  if (swTwo) {
-    ptr += "<p>Switch #2 : Status: ON</p><input type=\"button\" value=\"OFF\" onclick=\"window.location.href='/swtwo_off'\">\n";
-  }
-  else {
-    ptr += "<p>Switch #2 : Status: OFF</p><input type=\"button\" value=\"ON\" onclick=\"window.location.href='/swtwo_on'\">\n";
-  }
-  ptr += "</form>\n";
-  ptr += "</body>\n";
-  ptr += "</html>\n";
+  if (swTwo||swTwoState)
+    ptr += "<p>Switch #2 : Status: OFF</p><input type='button' value='ON' onclick=\"window.location.href='/swtwo_off'\">\n";
+  else
+    ptr += "<p>Switch #2 : Status: ON</p><input type='button' value='OFF' onclick=\"window.location.href='/swtwo_on'\">\n";
+  ptr += "</form>\n</body>\n</html>\n";
   return ptr;
 }
